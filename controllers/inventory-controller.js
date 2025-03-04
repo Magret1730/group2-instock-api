e1fdfe84bb85f123f77899707669d854f678da00
digibrigi <brigid@live.ca>
2025-03-04 18:42:52 -0500

    API to get list of all inventory

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,5 +1,14 @@
 import initKnex from "knex";
-// import configuration from "../knexfile.js";
-// const knex = initKnex(configuration);
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
 
-export { }
+const index = async (_req, res) => {
+    try {
+        const data = await knex("inventory");
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(400).send(`Error retrieving inventory: ${err}`);
+    }
+}
+
+export { index }
