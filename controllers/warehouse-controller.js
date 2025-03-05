79b5afde6b9be9dfee987a64f5b18e26555a9f73
Karen <karen12ngo@gmail.com>
2025-03-05 00:14:27 -0500

    implement api to get list of all inventory based on selected warehouse

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -3,12 +3,32 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 const index = async (_req, res) => {
-    try {
-        const data = await knex("warehouses");
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(400).send(`Error retrieving Warehouses: ${err}`);
-    }
-}
+  try {
+    const data = await knex("warehouses");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+  }
+};
 
-export { index }
+const getInventories = async (req, res) => {
+  try {
+    const inventories = await knex("inventories")
+      .where("inventories.warehouse_id", req.params.id)
+      .join("warehouses", "warehouses.id", "inventories.warehouse_id")
+      .select(
+        "inventories.id",
+        "inventories.item_name",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      );
+    res.status(200).json(inventories);
+  } catch (err) {
+    res.status(404).send({
+      message: `Unable to retrieve warehouse with ID ${req.params.id}`,
+    });
+  }
+};
+
+export { index, getInventories };
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import * as warehouseController from '../controllers/warehouse-controller.js';
+import express from "express";
+import * as warehouseController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
 router.route("/").get(warehouseController.index);
-
+router.route("/:id/inventories").get(warehouseController.getInventories);
 
 export default router;
