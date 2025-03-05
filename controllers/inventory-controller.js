9579a2ec3560f3fa438be7df844d12a943afd966
digibrigi <brigid@live.ca>
2025-03-04 19:08:16 -0500

    Added warehouse name to response data

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -4,7 +4,15 @@ const knex = initKnex(configuration);
 
 const index = async (_req, res) => {
     try {
-        const data = await knex("inventory");
+        const data = await knex("inventories")
+        .join("warehouses", "warehouses.id", "inventories.warehouse_id")
+        .select("inventories.id",
+            "warehouses.warehouse_name",
+            "inventories.item_name",
+            "inventories.description",
+            "inventories.category",
+            "inventories.status",
+            "inventories.quantity");
         res.status(200).json(data);
     } catch (err) {
         res.status(400).send(`Error retrieving inventory: ${err}`);
