297d8fe13ec84aa0cc3ec2ceb0aa0c2b8bd90e3d
Karen <karen12ngo@gmail.com>
2025-03-05 00:27:38 -0500

    implement api to delete a selected warehouse

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -3,12 +3,31 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 const index = async (_req, res) => {
-    try {
-        const data = await knex("warehouses");
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(400).send(`Error retrieving Warehouses: ${err}`);
-    }
-}
+  try {
+    const data = await knex("warehouses");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+  }
+};
 
-export { index }
+const remove = async (req, res) => {
+  try {
+    const warehouseDeleted = await knex("warehouses")
+      .where({ id: req.params.id })
+      .delete();
+
+    if (warehouseDeleted === 0) {
+      return res
+        .status(404)
+        .json({ message: `Warehouse with ID ${req.params.id} not found` });
+    }
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(500).json({
+      message: `Unable to delete warehouse with ID ${req.params.id}`,
+    });
+  }
+};
+
+export { index, remove };
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import * as warehouseController from '../controllers/warehouse-controller.js';
+import express from "express";
+import * as warehouseController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
 router.route("/").get(warehouseController.index);
-
+router.route("/:id").delete(warehouseController.remove);
 
 export default router;
