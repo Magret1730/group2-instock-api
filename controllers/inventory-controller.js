a5ecafa233cbea72cbb2a1011bf47639a3622d22
Magret <belloabiodun17@gmail.com>
2025-03-04 21:17:45 -0330

    Implement API to GET a Single Inventory Item

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -19,4 +19,32 @@ const index = async (_req, res) => {
     }
 }
 
-export { index }
+const findOne = async (req, res) => {
+    try {
+        const inventoryFound = await knex("inventories")
+            .where("inventories.id", req.params.id)
+            .join("warehouses", "warehouses.id", "inventories.warehouse_id")
+            .select("inventories.id",
+                "warehouses.warehouse_name",
+                "inventories.item_name",
+                "inventories.description",
+                "inventories.category",
+                "inventories.status",
+                "inventories.quantity");
+
+        if (inventoryFound.length === 0) {
+            return res.status(404).json({
+                message: `Inventory with ID ${req.params.id} not found` 
+            });
+        }
+
+        const inventoryData = inventoryFound[0];
+        res.json(inventoryData);
+    } catch (err) {
+        res.status(500).json({
+            message: `Unable to retrieve inventory data for inventory with ID ${req.params.id}`,
+        });
+    }
+}
+
+export { index, findOne }
